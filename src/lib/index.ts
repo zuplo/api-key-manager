f487ef10f224fa8f169c00bad1d046128b1ebf78
Josh Twist <josh@molmorg.com>
2023-08-02 21:33:17 -0700

    changed to default export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@ import ApiKeyManager from "../components/ApiKeyManager";
 
 // TODO - just trying to get this working right now, not sure this should be 'default' or export _type_ is best approach
 
-export { ApiKeyManager };
+export default ApiKeyManager;
 export type {
   ApiKeyManagerProvider,
   Consumer,
