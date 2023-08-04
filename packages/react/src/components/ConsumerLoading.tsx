e73e1fd3dab6e4488e2957da59b6429e3db450e8
Adrian Machado <amachado@uwaterloo.ca>
2023-08-04 10:54:27 -0700

    Fix shimmer #15
    * Fix shimmer
    
    * Change colors

--- packages/react/src/components/ConsumerLoading.tsx
+++ packages/react/src/components/ConsumerLoading.tsx
@@ -2,11 +2,11 @@ import { EllipsisVerticalIcon } from "../icons";
 
 const ConsumerLoading = () => {
   return (
-    <div className="rounded-lg bg-gray-200 border-gray-500 border mb-5">
-      <div className="flex flex-row justify-between items-center">
-        <div className="h-4 m-5 ml-4 w-44 animate-pulse rounded-lg bg-gray-300" />
-        <div className="p-4 opacity-50">
-          <EllipsisVerticalIcon className="h-4 w-4" />
+    <div className="rounded-lg bg-slate-50 border-zinc-200 border mb-5">
+      <div className="flex flex-row border-b border-zinc-200 justify-between items-center">
+        <div className="h-4 m-5 mb-[22px] ml-4 w-44 animate-pulse rounded-lg bg-gray-300" />
+        <div className="m-3 mt-5 mb-[18px] mr-4 opacity-50">
+          <EllipsisVerticalIcon className="h-5 w-5" />
         </div>
       </div>
       <div className="bg-white flex flex-col rounded-b-lg p-4 py-2">
@@ -19,13 +19,9 @@ const ConsumerLoading = () => {
 const KeyLoading = () => {
   return (
     <div className="flex w-full justify-between">
-      <div className="h-4 my-2 w-96 animate-pulse rounded-lg bg-gray-400" />
+      <div className="h-4 mt-6 mb-8 w-[440px] animate-pulse rounded-lg bg-gray-300" />
     </div>
   );
 };
 
 export default ConsumerLoading;
-
-// Ellipsis has roll active keys
-// When key has an expiry, you should expose delete option
-// Show shimmer for initial load, spinners for operations
